211e05f784ff468eb9d35fc33a135a2583279811
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-03 23:52:30 +0100

    rtc,ds3107 api prototype

--- stm32f4xx_drivers/bsp/ds1307.c
+++ stm32f4xx_drivers/bsp/ds1307.c
@@ -1,5 +1,26 @@
-/*
- * created ds1307.c on 1 Jul 2023 by nahl - (github.com/noargs)
- */
+#include "ds1307.h"
 
+uint8_t ds1307_init(void)
+{
 
+}
+
+void ds1307_set_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_get_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_set_current_date(RTC_date_t *rtc_date)
+{
+
+}
+
+void ds1307_get_current_date(RTC_date_t *rtc_date)
+{
+
+}
--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h
@@ -22,52 +22,52 @@ typedef struct {
 }GPIO_Handle_t;
 
 // @GPIO_PIN_NUMBERS
-#define GPIO_PIN_NO_0                               0
-#define GPIO_PIN_NO_1                               1
-#define GPIO_PIN_NO_2                               2
-#define GPIO_PIN_NO_3                               3
-#define GPIO_PIN_NO_4                               4
-#define GPIO_PIN_NO_5                               5
-#define GPIO_PIN_NO_6                               6
-#define GPIO_PIN_NO_7                               7
-#define GPIO_PIN_NO_8                               8
-#define GPIO_PIN_NO_9                               9
-#define GPIO_PIN_NO_10                              10
-#define GPIO_PIN_NO_11                              11
-#define GPIO_PIN_NO_12                              12
-#define GPIO_PIN_NO_13                              13
-#define GPIO_PIN_NO_14                              14
-#define GPIO_PIN_NO_15                              15
+#define GPIO_PIN_NO_0                    0
+#define GPIO_PIN_NO_1                    1
+#define GPIO_PIN_NO_2                    2
+#define GPIO_PIN_NO_3                    3
+#define GPIO_PIN_NO_4                    4
+#define GPIO_PIN_NO_5                    5
+#define GPIO_PIN_NO_6                    6
+#define GPIO_PIN_NO_7                    7
+#define GPIO_PIN_NO_8                    8
+#define GPIO_PIN_NO_9                    9
+#define GPIO_PIN_NO_10                   10
+#define GPIO_PIN_NO_11                   11
+#define GPIO_PIN_NO_12                   12
+#define GPIO_PIN_NO_13                   13
+#define GPIO_PIN_NO_14                   14
+#define GPIO_PIN_NO_15                   15
 
 
 // @GPIO_PIN_MODES
 // GPIO pins possible modes - [Reference Manual page:281]
-#define GPIO_MODE_IN                                0
-#define GPIO_MODE_OUT                               1
-#define GPIO_MODE_ALTFN                             2
-#define GPIO_MODE_ANALOG                            3
-#define GPIO_MODE_IT_FT                             4 // input falling-edge trigger
-#define GPIO_MODE_IT_RT                             5 // input rising-edge trigger
-#define GPIO_MODE_IT_RFT                            6 // input rising-falling-edge trigger
+#define GPIO_MODE_IN                     0
+#define GPIO_MODE_OUT                    1
+#define GPIO_MODE_ALTFN                  2
+#define GPIO_MODE_ANALOG                 3
+#define GPIO_MODE_IT_FT                  4 // input falling-edge trigger
+#define GPIO_MODE_IT_RT                  5 // input rising-edge trigger
+#define GPIO_MODE_IT_RFT                 6 // input rising-falling-edge trigger
 
 
 // GPIO pin possible output types
-#define GPIO_OP_TYPE_PP                             0 // output type push-pull
-#define GPIO_OP_TYPE_OD                             1 // output type open-drain
+#define GPIO_OP_TYPE_PP                  0 // output type push-pull
+#define GPIO_OP_TYPE_OD                  1 // output type open-drain
 
 
 // @GPIO_PIN_SPEED
 // GPIO pin possible output speeds
-#define GPIO_SPEED_LOW                              0
-#define GPIO_SPEED_MEDIUM                           1
-#define GPIO_SPEED_FAST                             2
-#define GPIO_SPEED_HIGH                             3
+#define GPIO_SPEED_LOW                   0
+#define GPIO_SPEED_MEDIUM                1
+#define GPIO_SPEED_FAST                  2
+#define GPIO_SPEED_HIGH                  3
 
 
 // GPIO pin pull up and pull down
-#define GPIO_NO_PUPD                                0 // no pull-up, pull-down
-#define GPIO_PU                                     1 // pull-up
-#define GPIO_PD                                     2 // pull-down
+#define GPIO_NO_PUPD                     0 // no pull-up, pull-down
+#define GPIO_PU                          1 // pull-up
+#define GPIO_PD                          2 // pull-down
 
 
 
