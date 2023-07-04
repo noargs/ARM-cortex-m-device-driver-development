e28556dabcf12ee711bc5cd6ed200c8728d64559
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-04 19:31:31 +0100

    bcd, binary conversion api implem

--- stm32f4xx_drivers/bsp/ds1307.c
+++ stm32f4xx_drivers/bsp/ds1307.c
@@ -8,6 +8,8 @@ static void ds1307_i2c_pin_config(void);
 static void ds1307_i2c_config(void);
 static void ds1307_write(uint8_t value, uint8_t reg_addr);
 static uint8_t ds1307_read(uint8_t reg_addr);
+static uint8_t bcd_to_binary(uint8_t value);
+static uint8_t binary_to_bcd(uint8_t value);
 
 I2C_Handle_t ds1307_i2c_handle;
 
@@ -166,7 +168,30 @@ static uint8_t ds1307_read(uint8_t reg_addr)
   return data;
 }
 
+static uint8_t binary_to_bcd(uint8_t value)
+{
+  uint8_t m, n;
+  uint8_t bcd;
 
+  bcd = value;
+
+  if (value >= 10)
+  {
+	m = value / 10;
+	n = value % 10;
+	bcd = (uint8_t)((m << 4) | n);
+  }
+
+  return bcd;
+}
+
+static uint8_t bcd_to_binary(uint8_t value)
+{
+  uint8_t m, n;
+  m = (uint8_t)((value >> 4) * 10);
+  n = value & (uint8_t)0x0F;
+  return (m+n);
+}
 
 
 
