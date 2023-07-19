38286616bbc22626147c24dd5bfeab3eb7dcdfb5
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-19 23:36:43 +0100

    lcd init

---
+++ stm32f4xx_drivers/bsp/lcd.c
@@ -0,0 +1,76 @@
+#include "lcd.h"
+
+void lcd_init(void)
+{
+	//1. configure the GPIO pins used for LCD connections
+	GPIO_Handle_t lcd_signal;
+
+	lcd_signal.gpiox                               = LCD_GPIO_PORT;
+	lcd_signal.gpio_config.gpio_pin_mode           = GPIO_MODE_OUT;
+	lcd_signal.gpio_config.gpio_pin_op_type        = GPIO_OP_TYPE_PP;
+	lcd_signal.gpio_config.gpio_pin_pu_pd_control  = GPIO_NO_PUPD;
+	lcd_signal.gpio_config.gpio_pin_speed          = GPIO_SPEED_FAST;
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_RS;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_RW;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_EN;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_D4;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_D5;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_D6;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.gpio_config.gpio_pin_number         = LCD_GPIO_D7;
+	GPIO_Init(&lcd_signal);
+
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_EN, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D4, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D5, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D6, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, GPIO_PIN_RESET);
+
+	//2. LCD Initialisation
+
+	mdelay(40); // milliseconds delay
+
+	// RS = 0, For LCD command
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+
+	// RnW = 0, Writing to LCD
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+
+	write_4_bits(0x3); // (MSB) 0 0 1 1 (LSB)
+
+	mdelay(5);
+
+	write_4_bits(0x3);
+
+	udelay(150);
+
+	write_4_bits(0x3);
+
+	write_4_bits(0x2);
+
+}
+
+// writes 4 bits of data/command on to D4,D5,D6,D7 lines
+static void write_4_bits(uint8_t value)
+{
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D4, ((value >> 0) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D5, ((value >> 1) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D6, ((value >> 2) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, ((value >> 3) & 0x1) );
+
+	lcd_enable();
+}
+
--- stm32f4xx_drivers/bsp/lcd.h
+++ stm32f4xx_drivers/bsp/lcd.h
@@ -1,6 +1,20 @@
 #ifndef LCD_H_
 #define LCD_H_
 
+#include "stm32f407xx.h"
+
+// bsp exposed apis
+void lcd_init(void);
+
+// Application configurable items
+#define LCD_GPIO_PORT                    GPIOD
+#define LCD_GPIO_RS                      GPIO_PIN_NO_0
+#define LCD_GPIO_RW                      GPIO_PIN_NO_1
+#define LCD_GPIO_EN                      GPIO_PIN_NO_2
+#define LCD_GPIO_D4                      GPIO_PIN_NO_3
+#define LCD_GPIO_D5                      GPIO_PIN_NO_4
+#define LCD_GPIO_D6                      GPIO_PIN_NO_5
+#define LCD_GPIO_D7                      GPIO_PIN_NO_6
 
 
 #endif /* LCD_H_ */
