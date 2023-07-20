de81d6b20aaf91442ead3d96e0b44b6280a2bbf6
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-20 15:34:24 +0100

    lcd command macros

--- stm32f4xx_drivers/bsp/lcd.h
+++ stm32f4xx_drivers/bsp/lcd.h
@@ -17,5 +17,12 @@ void lcd_send_command(uint8_t cmd);
 #define LCD_GPIO_D6                      GPIO_PIN_NO_5
 #define LCD_GPIO_D7                      GPIO_PIN_NO_6
 
+// LCD commands
+#define LCD_CMD_4DL_2N_5X8F              0x28 // 4bit Dataline, 2 lines, 5x8 dot font
+#define LCD_CMD_DON_CURON                0x0E // Display ON, Cursor ON
+#define LCD_CMD_INCADD                   0x06 // Increment the RAM Address
+#define LCD_CMD_DIS_CLEAR                0x01 // Display clear
+#define LCD_CMF_DIS_RETURN_HOME          0x02 // Display return home
+
 
 #endif /* LCD_H_ */
